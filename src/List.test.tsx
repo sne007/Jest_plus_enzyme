0c3c1bf4d6c1c7e7c3c04efc85379da4ebb790e8
Snehith Reddy Challa <challasnehith@gmail.com>
2022-01-07 15:01:30 -0800

    Add one more test Add one more test to add numbers 1 and 2.

--- src/List.test.tsx
+++ src/List.test.tsx
@@ -3,3 +3,7 @@ import { functions } from "./List";
 test("Add 2 + 2 is 4", () => {
   expect(functions.add(2, 2)).toEqual(4);
 });
+
+test("testing test", () => {
+  expect(functions.add(1, 2)).not.toBe(4);
+});
